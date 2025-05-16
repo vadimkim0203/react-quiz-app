6f413223a84776b8abaa18bdd0ccacff3fc9bb0e
Vadim Kim <vkim0203@gmail.com>
2025-05-16 12:12:20 +0900

    feat/add question timer

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -2,13 +2,28 @@ import { useState, useEffect } from 'react';
 
 export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
-  setTimeout(onTimeout, timeout);
 
   useEffect(() => {
-    setInterval(() => {
+    console.log('SETTING TIMEOUT');
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onTimeout, timeout]);
+
+  useEffect(() => {
+    console.log('SETTING INTERVAL');
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
-  return <progress id="question-time"></progress>;
+  return (
+    <progress id="question-time" max={timeout} value={remainingTime}></progress>
+  );
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompletedImage from '../assets/quiz-complete.png';
+import QuestionTimer from './QuestionTimer.jsx';
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState('');
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionsIndex = userAnswers.length;
 
   const isQuizFinished = activeQuestionsIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer,
+  ) {
+    setAnswerState('answered');
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+
+    setTimeout(() => {
+      if (selectedAnswer === QUESTIONS[activeQuestionsIndex].answers[0]) {
+        setAnswerState('correct');
+      } else {
+        setAnswerState('wrong');
+      }
+    }, 1000);
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => {
+    handleSelectAnswer(null);
+  }, [handleSelectAnswer]);
 
   if (isQuizFinished) {
     return (
@@ -29,6 +46,13 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          key={activeQuestionsIndex}
+          timeout={10000}
+          onTimeout={() => {
+            handleSkipAnswer;
+          }}
+        />
         <h2>{QUESTIONS[activeQuestionsIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
